daf9d2550e886092edd262f0af6008eb08446cf5
decendman <decendman@gmail.com>
2025-02-05 18:12:25 +0530

    Implemented function to fetch current weather data

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,15 @@
-document.getElementById('search-btn').addEventListener('click',
-    function() {
-     const city = document.getElementById('city-input').value;
-     console.log('City entered:', city);
-
-    });
+document.getElementById("search-btn").addEventListener("click", function () {
+  const city = document.getElementById("city-input").value;
+  fetchWeatherData(city);
+});
+async function fetchWeatherData(city) {
+  const apiKey = "ba29ac5ebf04302b4ef63ff52f4d799d";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log("Weather data:", data);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}

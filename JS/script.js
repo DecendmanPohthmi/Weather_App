5931cb0910b1e009fe2047e5ad48c101f78460b7
decendman <decendman@gmail.com>
2025-02-05 21:03:59 +0530

    Implemented function to fetch current weather data

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,15 @@ document.getElementById("search-btn").addEventListener("click", function () {
 });
 async function fetchWeatherData(city) {
   const apiKey = "ba29ac5ebf04302b4ef63ff52f4d799d";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log("Weather data:", data);
+    document.getElementById("city-name").innerHTML = `<strong>City:</strong> ${data.name}`
+    document.getElementById("humidity").innerHTML = `<strong>Humidity:</strong> ${data.main.humidity}`
+    document.getElementById("pressure").innerHTML = `<strong>Pressure:</strong> ${data.main.pressure}`
+    document.getElementById("temperature").innerHTML = `<strong>Temperature:</strong> ${data.main.temp}℃`
+    document.getElementById("weather-description").innerHTML = `<strong>Description:</strong> ${data.weather[0].description}`
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
